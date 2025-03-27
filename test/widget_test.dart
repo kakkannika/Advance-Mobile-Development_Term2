25ec5cee405b55ca89cdb415d2548e872b24f063
kakkannika <kakkannika484@gmail.com>
2025-03-27 19:58:57 +0700

    BLA-200- Start Code Week 7

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,8 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:week_3_blabla_project/main.dart';
 
-import 'package:advance_flutter/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
